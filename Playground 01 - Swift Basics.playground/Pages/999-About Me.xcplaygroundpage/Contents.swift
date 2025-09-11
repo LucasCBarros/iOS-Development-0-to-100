787edffb982117ab3804ee3faa0af09662b3de45
Lucas C Barros <Lucas_cb@icloud.com>
2025-09-11 14:48:47 -0700

    updating a few details

--- Playground 01 - Swift Basics.playground/Pages/999-About Me.xcplaygroundpage/Contents.swift
+++ Playground 01 - Swift Basics.playground/Pages/999-About Me.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //: [<< Summary](00-Introduction)\
 //: [< Previous](@previous)
 //: # 👋 About Me
-//: ## Welcome! I’m **Lucas Barros**, an iOS Developer and mentor.
+//: ## Hello world! I’m **Lucas Barros**, iOS Developer and mentor.
 //: ![My Photo](Profile_Photo.png)
 //:
 //: ## 👨🏻‍💻 Professional Info
@@ -10,11 +10,14 @@
 //: - 💻 Experience: +6 years in iOS, Swift, SwiftUI & UIKit
 //: - 🚀 Interests: Mobile architecture, mentoring, performance optimization
 //:
+//: ---
 //: ## 🌐 Links
+//: If you enjoyed this material you can find more at 🪽[iOS Development 0 to 100](https://github.com/LucasCBarros/iOS-Development-0-to-100)\
+//: Or learn more about other projects I'm working on:
 //: - 🔗 [GitHub](https://github.com/LucasCBarros)
 //: - 💼 [LinkedIn](https://www.linkedin.com/in/lucascbarros/)
 //: - 🌎 [Portfolio Website](https://lucascbtech.wixsite.com/kodeshark)
-//: - 📧 Email: LucasCavalcanteDeBarros@gmail.com
+//: - 📧 [LucasCavalcanteDeBarros@gmail.com](LucasCavalcanteDeBarros@gmail.com)
 //: ---
 //: ## ✨ Personal Note ✨
 //: > *“I believe great code is not just about syntax, but about clarity, collaboration, and impact.”*
